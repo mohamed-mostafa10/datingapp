417632b3b23a9ebac79412650acfd09293103715
mohamed-mostafa10 <mohamed.mostafa@easacc.com>
2021-09-29 16:06:30 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -53,6 +54,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+            .WithOrigins("Https://localhost:4200"));
 
             app.UseAuthorization();
 
